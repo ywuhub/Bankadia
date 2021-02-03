c910a5080b622db4631b100be11bcda0e97025d5
Allan Wu <allan.wu6@gmail.com>
2021-02-03 23:29:08 +1100

    Added Guide Section of Homepage

--- src/App.js
+++ src/App.js
@@ -18,16 +18,41 @@ function App() {
       <div class="login">
         <div class="row">
           <div class="column">
-            <h3 class="greeting">Welcome to Bankadia!</h3>
+            <h3 class="greeting bubbles">Welcome to Bankadia!</h3>
             <blockquote class="quote">We buy high, so you can sell low.</blockquote>
           </div>
-          <div class="column">Test</div>
         </div>
       </div>
 
-      <div class="footer">
-        <p>Footer</p>
-      </div> 
+      <div class="guide">
+        <div class="row">
+            <div class="column">
+              <a href="#account-options" class="guide_anchor">Banking Options &#9654;</a>
+              <p>If you think our banking options are just like our competitors, you're right, because it's all about money, money, money.</p>
+            </div>
+            <div class="column">
+              <a href="#help-center" class="guide_anchor">Financial Help Center &#9654;</a>
+              <p>Learn how to navigate the world of financial jargon through our comprehensive gluten free articles.</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="introduction">
+        <div class="row">
+          <div class="column">
+
+          </div>
+          <div class="column">
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-basic">
+        <footer>
+            <div class="social"><a href="https://github.com/ywuhub" target="_blank" rel="noreferrer"><i class="icon ion-social-instagram"></i></a></div>
+            <p class="copyright">Bankadia © | Allan Wu | Inspired By <a href="https://www.youtube.com/channel/UCm22FAXZMw1BaWeFszZxUKw" target="_blank" rel="noreferrer">Kitboga</a></p>
+        </footer>
+    </div> 
     </div>
   );
 }
