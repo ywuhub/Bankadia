44cdf63451b25b34de25b9a60601709d0801fe30
Allan Wu <allan.wu6@gmail.com>
2021-02-09 23:45:53 +1100

    Starting the Contact Form and connecting the pages together

---
+++ src/contact.js
@@ -0,0 +1,15 @@
+// Import Componments
+import Header from './header.js'
+import Footer from './footer.js';
+
+function Contact() {
+    return (
+        <div className="contact">
+            <Header />
+            <h1>Hello World</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default Contact
--- src/header.js
+++ src/header.js
@@ -1,18 +1,21 @@
+import { BrowserRouter, Link } from 'react-router-dom';
 import './header.css';
 
 function Header() {
     return (
-        <div  className="header">
+        <div className="header">
             <div class="logo">
                 <h1 id="bank">💸 Bankadia</h1>
             </div>
                 
             <div class="topnav">
-                <a href="#personal">Personal</a>
-                <a href="#business">Business</a>
-                <a href="#investing">Investing</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <BrowserRouter>
+                    <Link to="">Personal</Link>
+                    <Link to="">Business</Link>
+                    <Link to="">Investing</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="">About</Link>
+                </BrowserRouter>
             </div>
         </div>
     )
