1e2feda7c5c20a15b78fb84b0b72d466c5deb1dd
Allan Wu <allan.wu6@gmail.com>
2021-02-10 23:57:49 +1100

    Continuing to learn about React Routing

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,11 @@
 // Import Componments
-import Header from './header.js'
-import Footer from './footer.js';
+//import Header from './header.js'
+//import Footer from './footer.js';
 
 function Contact() {
     return (
         <div className="contact">
-            <Header />
             <h1>Hello World</h1>
-            <Footer />
         </div>
     )
 }
--- src/header.js
+++ src/header.js
@@ -1,5 +1,7 @@
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './header.css';
+
+import Contact from "./contact.js"
 
 function Header() {
     return (
@@ -8,15 +10,22 @@ function Header() {
                 <h1 id="bank">💸 Bankadia</h1>
             </div>
                 
-            <div class="topnav">
-                <BrowserRouter>
+            <BrowserRouter>
+                <div class="topnav">
+                    <Link to="">Home</Link>
                     <Link to="">Personal</Link>
                     <Link to="">Business</Link>
                     <Link to="">Investing</Link>
                     <Link to="/contact">Contact</Link>
                     <Link to="">About</Link>
-                </BrowserRouter>
-            </div>
+
+                    <Switch>
+                        <Route exact path="/contact" component={Contact}>
+                            <Contact />
+                        </Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
