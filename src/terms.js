d6ae4758a353214ac933090db070bbf0638e7bd4
Allan Wu <allan.wu6@gmail.com>
2021-02-27 23:00:45 +1100

    Added noreferrer to anchor tags

--- src/terms.js
+++ src/terms.js
@@ -34,7 +34,7 @@ function Terms() {
 <p><strong>Third-party Social Media Service</strong> means any services or content (including data, information, products or services) provided by a third-party that may be displayed, included or made available by the Service.</p>
 </li>
 <li>
-<p><strong>Website</strong> refers to Bankadia Financials, accessible from <a href="https://bankadia-financials.web.app/" rel="external nofollow noopener" target="_blank">https://bankadia-financials.web.app/</a></p>
+<p><strong>Website</strong> refers to Bankadia Financials, accessible from <a href="https://bankadia-financials.web.app/" rel="external nofollow noopener noreferrer" target="_blank">https://bankadia-financials.web.app/</a></p>
 </li>
 <li>
 <p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
