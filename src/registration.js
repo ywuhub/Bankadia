b79e615cb5557331dd4c084bd9568a499ce16806
Allan Wu <allan.wu6@gmail.com>
2021-02-27 22:38:24 +1100

    Update: Registration Form completed
    Formatting is ignored to save time, but it should be functional.

--- src/registration.js
+++ src/registration.js
@@ -28,18 +28,18 @@ function Register() {
                             </div>
                             <div class="register-row">
                                 <div class="register-col">
-                                    <label for="email" class="account_label">Email:</label> <input type="text" name="email"/><br/>
+                                    <label for="email" class="account_label">Email:</label> <input type="text" name="email" required/><br/>
                                 </div>
                                 <div class="register-col">
-                                    <label for="password" class="account_label">Password:</label> <input type="password" name="password"/>
+                                    <label for="password" class="account_label">Password:</label> <input type="password" name="password" required/>
                                 </div>
                             </div>
                             <div class="register-row">
                                 <div class="register-col">
-                                    <label for="confirm_email" class="account_label">Confirm Email:</label> <input type="text" name="confirm_email"/><br/>
+                                    <label for="confirm_email" class="account_label">Confirm Email:</label> <input type="text" name="confirm_email" required/><br/>
                                 </div>
                                 <div class="register-col">
-                                    <label for="confirm_password" class="account_label">Confirm Password:</label> <input type="password" name="confirm_password"/><br/>
+                                    <label for="confirm_password" class="account_label">Confirm Password:</label> <input type="password" name="confirm_password" required/><br/>
                                 </div>
                             </div>                                                 
                         </div>
@@ -50,11 +50,55 @@ function Register() {
                         </legend>
                         <div class="personal-details">
                             <div class="register-row">
+                                <div class="register-col-long">
+                                    <label for="name" class="account_label">Name:</label>
+                                    <select name="name_prefix" id="name_prefix">
+                                        <option value="mr">Mr</option>
+                                        <option value="mrs">Mrs</option>
+                                        <option value="ms">Ms</option>
+                                        <option value="dr">Dr</option>
+                                    </select>
+                                    <input type="text" name="fname" id="fname" placeholder="First Name" required/><input type="text" name="lname" id="lname" placeholder="Last Name" required/><br/>
+                                </div>
+                            </div>
+                            <div class="register-row">
+                                <div class="register-col-long">
+                                    <label for="birthday" class="account_label">Date of Birth:</label>
+                                    <input type="date" name="birthday" id="birthday" required/><br/>
+                                </div>
+                            </div>
+                            <div class="register-row">
+                                <div class="register-col-long">
+                                    <label for="phone">Enter your mobile number: </label>
+                                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}\s[0-9]{4}\s[0-9]{4}" placeholder="Format: xxx xxxx xxxx" required/>
+                                </div>
+                            </div>
+                            <div class="register-row">
                                 <div class="register-col">
-                                    <label for="fname" class="account_label">Name:</label> <input type="text" name="fname"/><br/>
+                                    <label for="street_address">Street Address: </label>
+                                    <input type="text" id="street_address" name="street_address" placeholder="Enter your address..." required/>
                                 </div>
                                 <div class="register-col">
-                                    <label for="confirm_password" class="account_label">Confirm Password:</label> <input type="password" name="confirm_password"/><br/>
+                                    <label for="city" id="city-label">City/Suburb: </label>
+                                    <input type="text" id="city" name="city" placeholder="Enter your city..." required/>
+                                </div>
+                            </div>
+                            <div class="register-row">
+                                <div class="register-col">
+                                    <label for="state">State: </label>
+                                    <select name="state_list" id="state_list">
+                                        <option value="NSW">New South Wales</option>
+                                        <option value="QLD">Queensland</option>
+                                        <option value="SA">South Australia</option>
+                                        <option value="NT">Northen Territory</option>
+                                        <option value="WA">Western Australia</option>
+                                        <option value="TAS">Tasmania</option>
+                                        <option value="VIC">Victoria</option>
+                                    </select>
+                                </div>
+                                <div class="register-col">
+                                    <label for="postcode" id="postcode-label">Postcode: </label>
+                                    <input type="text" id="postcode" name="postcode" pattern="[0-9]{4}" placeholder="Enter your postcode..." required/>
                                 </div>
                             </div>
                         </div>  
@@ -63,6 +107,13 @@ function Register() {
                         <legend>
                             <h3>Terms and Confirmation</h3>
                         </legend>
+                        <div class="register-col-long">
+                            <input type="checkbox" id="check_1" name="check_1" value="check_1" required/>
+                            <label for="check_1"> Check this box if you agree to our terms and conditions.</label><br/>
+                            <input type="checkbox" id="check_2" name="check_2" value="check_2" required/>
+                            <label for="check_2"> Check this box if the above information is 100% true and you agree to give us all your money if this is not the case.</label><br/>
+                            <button class="sign_up"><span>Sign up for free</span></button>
+                        </div>
                     </fieldset>
                 </form>
             </div>
